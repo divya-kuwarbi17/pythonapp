6e0f6bfb1cc57d6984d4cf6d43f54fb4988bca5b
divya-kuwarbi17 <divyakuwarbi@gmail.com>
2025-07-03 11:29:36 +0530

    checking the test branch

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return "Hello world I'm in lechler india pvt ltd."
+    return "Hello world I'm in lechler india pvt ltd. and this is the staging branch"
 
